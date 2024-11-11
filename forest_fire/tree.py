c1f74d539eb167d0111c6eacaf3b1dec4bc8dc93
Suelen Luiz <al.suelen.luiz@impatech.org.br>
2024-11-11 13:42:58 -0300

    Recados do Tomaz e da Suelen:     Implementamos um parâmetro chamado "reprod_speed" que controla a velocidade com que a floresta se reproduz     e a interface visual para que o usuário o controle. Além disso, ajustamos os comentários para facilitar a leitura     do código.

--- forest_fire/tree.py
+++ forest_fire/tree.py
@@ -3,22 +3,24 @@ import random
 
 
 class Tree(mesa.Agent):
-    def __init__(self, unique_id, model, pos, tree_density):
+    def __init__(self, unique_id, model, pos, tree_density, reprod_speed=1):
         super().__init__(unique_id, model)
         self.pos = pos
         self.status = "Fine"
-        # foi adicionado o argumento "tree_density, que é a densidade inicial da floresta"
+        # Foi adicionado o argumento "tree_density", que é a densidade inicial da floresta.
+        # Foi adicionado o argumento "reprod_speed", que é um parâmetro que controla a velocidade de reprodução das árvores.
         self.tree_density = tree_density
+        self.reprod_speed = reprod_speed
     
 
     def can_grow(self, cord):
-        # essa função previne que o reflorestamento ultrapasse a densidade inicial
+        # Essa função previne que o reflorestamento ultrapasse a densidade inicial.
         n1_number = 0
         n2_number = 0
         n1_tree = 0
         n2_tree = 0
         visited_cells = set()
-        # é feita uma busca em largura para estimar a "densidade local"
+        # É feita uma busca em largura para estimar a "densidade local".
         if cord.status == 'Burned':
             for neighbor1 in self.model.grid.iter_neighbors(cord.pos, True):
                 if neighbor1 not in visited_cells:
@@ -33,40 +35,40 @@ class Tree(mesa.Agent):
                         n2_number += 1
                         if neighbor2.status == 'Fine':
                             n2_tree += 1
-            # a escolha desse return se dá para tentar restringir as limitações de estarmos tratando com inteiros
-            return (n1_tree < self.tree_density * n1_number) and (n2_tree < self.tree_density * n2_number)
+            # A escolha desse return se dá para tentar restringir as limitações de estarmos tratando com inteiros
+            return (n1_tree < self.tree_density * n1_number) and (n2_tree < self.tree_density * n2_number) 
         return False
 
 
     def tree_reproduction(self):
         neighbors1_number = 0
         neighbors2_number = 0
-        neighbors_tree = 1
+        neighbors_tree_fine = 1 
         visited_cells = set()
-        # aqui a lógica é parecida, são feitas duas bfs
-        # a primeira estima uma probabilidade de nascimento de uma nova árvora, e a segunda realiza o reflorestamento
+        # Aqui a lógica é parecida com a da função anterior, são feitas duas bfs.
+        # A primeira estima uma probabilidade de nascimento de uma nova árvore, e a segunda realiza o reflorestamento.
         if self.status == 'Fine':
             for neighbor1 in self.model.grid.iter_neighbors(self.pos, True):
                 neighbors1_number += 1
                 if neighbor1 not in visited_cells and neighbor1.status == 'Fine':
                     visited_cells.add(neighbor1)
-                    neighbors_tree += 1
+                    neighbors_tree_fine += 1
                 for neighbor2 in self.model.grid.iter_neighbors(neighbor1.pos, True):
                     neighbors2_number += 1
                     if neighbor2 not in visited_cells and neighbor2.status == 'Fine':
                         visited_cells.add(neighbor2)
-                        neighbors_tree += 1
+                        neighbors_tree_fine += 1
             visited_cells = set()
-            # cálculo de probabilidade
-            neighbors_number = neighbors1_number + neighbors2_number + 1
-            if (neighbors_tree/neighbors_number) < self.tree_density:
+            # Cálculo da velocidade de reflorestamento
+            neighbors_number = neighbors1_number + neighbors2_number + 1 
+            if (neighbors_tree_fine/neighbors_number) < self.tree_density:
                 expected_trees = (neighbors_number *
-                                  self.tree_density) - neighbors_tree
+                                  self.tree_density) - neighbors_tree_fine 
                 n1_reproduction_rate = (
-                    expected_trees/(2 * neighbors1_number * neighbors_number))
+                    expected_trees/(neighbors1_number * neighbors_number))*self.reprod_speed
                 n2_reproduction_rate = (
-                    expected_trees/(2 * neighbors2_number * neighbors_number))
-                # reprodução das árvores
+                    expected_trees/(neighbors2_number * neighbors_number))*self.reprod_speed
+                # Reprodução das árvores
                 for neighbor1 in self.model.grid.iter_neighbors(self.pos, True):
                     if neighbor1 not in visited_cells:
                         visited_cells.add(neighbor1)
@@ -90,9 +92,7 @@ class Tree(mesa.Agent):
 
 '''
 Recados do Tomaz e da Suelen:
-a implementação de reflorestamento funciona, porém no processo de desenvolvimento 
-do reflorestamento, provavelmente sobrepusermos alguns métodos para estimar a 
-suscetibilidade do nascimento de árvores. Em breve isso será revisto e corrigido.
-Além disso, as redundâncias como os for's monstuosos e os if's infinitos também
-serão analisados com cuidado, para uma melhor compreensão do código.
+    Implementamos um parâmetro chamado "reprod_speed" que controla a velocidade com que a floresta se reproduz
+    e a interface visual para que o usuário o controle. Além disso, ajustamos os comentários para facilitar a leitura
+    do código.
 '''
