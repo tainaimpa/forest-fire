398137a41023578d95a529f37dc260c841315feb
Bianca Zavadisk <bianca.zavadisk@gmail.com>
2024-11-15 17:23:37 -0300

    Organização do commit anterior com a criação do agente obstáculo e dos obstáculos: rio, lago e corredor

---
+++ forest_fire/obstacles.py
@@ -0,0 +1,34 @@
+from mesa import Agent
+
+
+class Obstacle(Agent): # Create a general obstacle
+    def __init__(self, unique_id, model, pos):
+        super().__init__(unique_id, model)
+        self.burnable = False
+        self.pos
+        self.status = "Obstacle"
+        
+class River(Obstacle):
+    def __init__(self, unique_id, model, pos):
+        super().__init__(unique_id, model)
+        self.burnable = True
+        self.status = 'Flowing'
+        
+    def step(self):
+        if self.status == 'Steaming' and self.width != 1:
+            for neighbor in self.model.grid.iter_neighbors(self.width, True):
+                if neighbor.status == "Flowing":
+                    neighbor.status = "Steaming"
+            self.status = "Evaporated"
+            
+class Lake(Obstacle):
+    def __init__(self, unique_id, model, pos):
+        super().__init__(unique_id, model, pos)
+        self.burnable = False
+
+class Corridor(Obstacle):
+    def __init__(self, unique_id, model, pos):
+        super().__init__(unique_id, model, pos)
+        self.burnable = True
+        self.spread_rate = 2.0
+        self.status = "Corridor"
